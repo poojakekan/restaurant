f815c57c6d3f3096e1e6160836474baf639b39ef
Pooja Kekan <35685820+poojakekan@users.noreply.github.com>
2024-06-13 14:02:59 +0530

    fourth commit

---
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -0,0 +1,46 @@
+import React from "react";
+import classes from "./MealItemForm.module.css";
+import Input from "../../UI/Input";
+import { useRef, useState } from "react";
+const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+
+  const [amountIsVaild, setAmountIsVaild] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsVaild(false);
+      return;
+    }
+    props.onAddToCart(enteredAmountNumber);
+  };
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        label="Amount"
+        ref={amountInputRef}
+        input={{
+          id: "amount",
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: 1,
+        }}
+      />
+      <button type="submit">+ Add</button>
+      {!amountIsVaild && <p>Please Enter vail input amount (1-5).</p>}
+    </form>
+  );
+};
+
+export default MealItemForm;
